5689ef86d9e206517865bf1bfb191387b283cc5a
Maverick <85990379+MaverickJames21@users.noreply.github.com>
2021-12-27 19:49:02 +0100

    learningVar

--- learn.js
+++ learn.js
@@ -1,3 +1,19 @@
-console.log('salut');
-console.log('salut');
-console.log('salut');
+let prenom  = "maverick";
+let nom     = "Hegi"
+
+// concaténation
+console.log('bonjour ' + prenom + ' ', nom,' !');
+
+// OPERATEURS
+
+// '%' modulo <=> 5/2 = 2,45343 restant 1
+// '**' exponentielle 5**2 = 25 
+
+// regrouper 2 variable avec un mot clé -> let
+let nombreUN = 8, nombreDeux = 4, result;
+result = (nombreUN + nombreDeux) *9; //108
+// incrémentation
+result++
+// décrémentation
+result--
+console.log(result + prenom + ' ', nom,' !');
